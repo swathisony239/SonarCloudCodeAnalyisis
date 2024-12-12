1ac8349f0813e44f49ac4f71a68c88b47370d6b8
swathisony239 <45678998+swathisony239@users.noreply.github.com>
2024-12-12 16:49:51 +0530

    Update scripts1.py

--- Sourcecode/scripts1.py
+++ Sourcecode/scripts1.py
@@ -1,6 +1,31 @@
-def add(a, b):
-    return a + b
+import os
+import requests
+import sys
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
+TOKEN= str(sys.argv[1])
+OWNER= str(sys.argv[2])
+REPO= str(sys.argv[3])
+workflowname= str(sys.argv[4])
+parameter1= str(sys.argv[5])
+parameter2 = str(sys.argv[6])
+
+print( "the toke value is")
+def trigger_workflow(workflowname,parameter1,parameter2):
+
+      headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {TOKEN}",
+      }
+
+      data = {
+        "event_type": workflowname,
+        "client_payload": {
+          'parameter1': parameter1,
+          'parameter2': parameter2
+        }
+      }
+
+      responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
+      print(responsevalue.content)
+
+trigger_workflow(workflowname,parameter1,parameter2)
